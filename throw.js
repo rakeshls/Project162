1496941500970315b9c2a3c5007a9e1d1af21b5c
rakeshls <73830663+rakeshls@users.noreply.github.com>
2022-07-16 18:22:56 +0530

    Add files via upload

---
+++ throw.js
@@ -0,0 +1,46 @@
+AFRAME.registerComponent("bowling-balls",{
+    init:function(){
+        this.throwBall()
+    },
+    throwBall:function(){
+        window.addEventListener("keydown",(e)=>{
+            if(e.key === 'a'){
+                var ball = document.createElement("a-entity")
+                ball.setAttribute("gltf-model", "./models/bowling_ball/scene.gltf")
+                ball.setAttribute("scale",{x:3,y:3,z:3})
+                var cam = document.querySelector("#camera")
+                position = cam.getAttribute("position")
+                ball.setAttribute("position",{
+                    x:position.x,
+                    y:position.y,
+                    z:position.z
+                })
+                ball.setAttribute("dynamic-body",{
+                    shape:"sphere",
+                    mass:"5"
+                })
+                var camera = document.querySelector("#camera").object3D
+                var direction =new THREE.Vector3()
+                camera.getWorldDirection(direction)
+                ball.setAttribute("velocity", direction.multiplyScalar(-10))
+                var scene =document.querySelector("#scene")
+                ball.addEventListener("collide", this.removeBall);
+                scene.appendChild(ball);
+            }
+        })
+    },
+    removeBall: function (e) {
+        var element = e.detail.target.el;
+        var elementHit = e.detail.body.el;
+        if (elementHit.id.includes("pin")) {
+          var impulse = new CANNON.Vec3(0,1,-15);
+          var worldPoint = new CANNON.Vec3().copy(
+            elementHit.getAttribute("position")
+          );
+          elementHit.body.applyForce(impulse, worldPoint);
+          element.removeEventListener("collide", this.removeBall);
+          var scene = document.querySelector("#scene");
+          scene.removeChild(element);
+        }
+    }
+})
